1efb87d563d0ec70b6aba26992c5fe3797ced603
Akshaysh14 <akshayshendkar14@gmail.com>
2021-10-23 15:52:40 +0530

    v0.0.1 Features - Integrate primeng and setup environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+const domain = 'http://127.0.0.1:8000'
 export const environment = {
-  production: false
+  production: false,
+  base_url: domain,
+  api_url: domain + '/api/v1'
 };
 
 /*
